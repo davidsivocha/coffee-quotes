f6c894cb9aa18c4cba8b97fa3dbcf5bec6f254ae
David Sivocha <david@sivocha.com>
2015-11-11 20:35:45 +0000

    Add GA Tracking Code

--- coffee-quotes/resources/views/quote.blade.php
+++ coffee-quotes/resources/views/quote.blade.php
@@ -9,7 +9,7 @@
     <meta property="og:type"               content="website">
     <meta property="og:title"              content="Coffee Quotes - Daily Quotes about Coffee">
     <meta property="og:description"        content="{{$quote->text}} - {{$quote->author}}">
-    <meta name="twitter:image"             content="http://coffeequotes.co.uk/img/{{$quote->background}}">
+    <meta property="og:image"              content="http://coffeequotes.co.uk/img/{{$quote->background}}">
     <meta name="twitter:card"              content="summary_large_image">
     <meta name="twitter:site"              content="@daelach">
     <meta name="twitter:creator"           content="@daelach">
@@ -28,5 +28,15 @@
         <p class="author">&mdash; {{$quote->author}} <a href="/{{$quote->id}}" title="Permalink">#</a></p>
     </div>
 </div>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69993349-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </body>
 </html>
